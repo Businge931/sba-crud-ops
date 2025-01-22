10772519842667312e2582501233f2f7a3b4ffb4
Businge931 <jimmybusinge9@gmail.com>
2025-01-22 17:27:52 +0300

    fix: rename aDB connection string

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,7 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgresql://admin:adminpassword@localhost:5432/api_gateway?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgresql://admin:adminpassword@localhost:5432/sba_crud_ops?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_OPEN_TIME", "15m"),
