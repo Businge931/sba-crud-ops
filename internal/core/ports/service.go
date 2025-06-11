3f6373594fa717962a80bd8209b7c93befa46ab2
Businge931 <busingebisanga99@gmail.com>
2025-06-11 15:44:01 +0300

    refactor: format interface definitions for improved readability in service.go

--- internal/core/ports/service.go
+++ internal/core/ports/service.go
@@ -6,25 +6,27 @@ import (
 	"github.com/Businge931/sba-crud-ops/internal/core/domain"
 )
 
-type OddsCreator interface {
-	CreateOdds(ctx context.Context, request domain.CreateOddsRequest) error
-}
+type (
+	OddsCreator interface {
+		CreateOdds(ctx context.Context, request domain.CreateOddsRequest) error
+	}
 
-type OddsRetriever interface {
-	ReadOdds(ctx context.Context, request domain.ReadOddsRequest) ([]domain.Odds, error)
-}
+	OddsRetriever interface {
+		ReadOdds(ctx context.Context, request domain.ReadOddsRequest) ([]domain.Odds, error)
+	}
 
-type OddsUpdater interface {
-	UpdateOdds(ctx context.Context, request domain.CreateOddsRequest) error
-}
+	OddsUpdater interface {
+		UpdateOdds(ctx context.Context, request domain.CreateOddsRequest) error
+	}
 
-type OddsDeleter interface {
-	DeleteOdds(ctx context.Context, request domain.DeleteOddsRequest) error
-}
+	OddsDeleter interface {
+		DeleteOdds(ctx context.Context, request domain.DeleteOddsRequest) error
+	}
 
-type OddsService interface {
-	OddsCreator
-	OddsRetriever
-	OddsUpdater
-	OddsDeleter
-}
+	OddsService interface {
+		OddsCreator
+		OddsRetriever
+		OddsUpdater
+		OddsDeleter
+	}
+)
