402f0a8e1575299fd69ab8bdf8073de2206dd448
Businge931 <busingebisanga99@gmail.com>
2025-06-11 15:42:37 +0300

    refactor: add GetBool function to retrieve boolean values from environment variables

--- internal/env/env.go
+++ internal/env/env.go
@@ -28,3 +28,18 @@ func GetInt(key string, fallback int) int {
 
 	return valAsInt
 }
+
+// GetBool retrieves a boolean value from environment variables or returns the fallback
+func GetBool(key string, fallback bool) bool {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsBool, err := strconv.ParseBool(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsBool
+}
